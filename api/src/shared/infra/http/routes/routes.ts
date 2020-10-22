45ed5040f7234169593bb7685e6453f31c61c66e
Gustavo Cândido <gustavocandidofranca@gmail.com>
2020-10-22 14:54:58 -0300

    feature fineshed

--- api/src/shared/infra/http/routes/routes.ts
+++ api/src/shared/infra/http/routes/routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import patientsRouter from '@modules/patient/infra/http/routes/patients.routes';
 import measurementsRouter from '@modules/measurements/infra/http/routes/measurements.routes';
 import profileRouter from '@modules/Profile/infra/http/routes/profile.routes';
+import professionalRouter from '@modules/professional/infra/http/routes/professional.routes';
 
 const router = Router();
 
 router.use('/api/patient', patientsRouter);
 router.use('/api/measurement', measurementsRouter);
 router.use('/api/profile', profileRouter);
+router.use('/api/professional',professionalRouter);
 
 
 export default router;
